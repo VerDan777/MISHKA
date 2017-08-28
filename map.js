8a459e05aca840b1251ce4c12861c63bc914a152
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-08-28 17:14:38 +0200

    stylelint installed

--- map.js
+++ map.js
@@ -1,9 +1,14 @@
-function initMap() {
+
+    window.initMap=function() {
+
     var location = {lat: 54.7104260, lng:20.4522140};
+    var stylemap = new google.maps.StyledMapType(require('./StyleMap.js'));
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
     center: location
-    });
+});
+   map.mapTypes.set('styled_map', stylemap);
+    map.setMapTypeId('styled_map'); 
     var marker = new google.maps.Marker({
         position: location,
         map: map
