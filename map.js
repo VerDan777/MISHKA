aee2e80c14b8d8b015f5dcc61bb6b4a765c531b0
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-09-01 12:47:32 +0200

    updating google map config

--- map.js
+++ map.js
@@ -4,12 +4,20 @@
     var location = {lat: 54.7104260, lng:20.4522140};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-    center: location
+        center: location
 });
-//    map.mapTypes.set('styled_map', stylemap);
-//     map.setMapTypeId('styled_map'); 
-//     var marker = new google.maps.Marker({
-//         position: location,
-//         map: map
-//     });
+        var imagemarker = new google.maps.MarkerImage(
+            './img/pin.png',
+            new google.maps.Size(100,100),
+            new google.maps.Point(0,0),
+            new google.maps.Point(16,32)
+        );
+            var marker = new google.maps.Marker({
+                draggable: false,
+                raiseOnDrag: false,
+                animation: google.maps.Animation.BOUNCE,
+                icon: imagemarker,
+                map: map,
+                position: location
+});
 }
