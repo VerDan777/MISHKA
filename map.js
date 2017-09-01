c077e045ecb96d0064acd4e5cd3e1f5e64dd8e73
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-09-01 12:33:28 +0200

    section contacts and footer base finished for tablets

--- map.js
+++ map.js
@@ -2,15 +2,14 @@
     window.initMap=function() {
 
     var location = {lat: 54.7104260, lng:20.4522140};
-    var stylemap = new google.maps.StyledMapType(require('./StyleMap.js'));
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
     center: location
 });
-   map.mapTypes.set('styled_map', stylemap);
-    map.setMapTypeId('styled_map'); 
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
+//    map.mapTypes.set('styled_map', stylemap);
+//     map.setMapTypeId('styled_map'); 
+//     var marker = new google.maps.Marker({
+//         position: location,
+//         map: map
+//     });
 }
